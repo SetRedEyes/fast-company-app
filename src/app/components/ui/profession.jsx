dd34d5efe0f971c6730a387f667a1db6564658b5
SetRedEyes <denidraven@gmail.com>
2022-01-24 12:10:55 +0100

    formatted

--- src/app/components/ui/profession.jsx
+++ src/app/components/ui/profession.jsx
@@ -1,21 +1,16 @@
-import React, { useEffect } from "react"
+import React from "react"
 import PropTypes from "prop-types"
-import { useDispatch, useSelector } from "react-redux"
+import { useSelector } from "react-redux"
 import {
     getProfessionById,
     getProfessionsLoadingStatus
 } from "../../store/professions"
-import { loadQualitiesList } from "../../store/qualities"
 
 const Profession = ({ id }) => {
-    const dispatch = useDispatch()
     const isLoading = useSelector(getProfessionsLoadingStatus())
 
     const prof = useSelector(getProfessionById(id))
 
-    useEffect(() => {
-        dispatch(loadQualitiesList())
-    }, [])
     if (!isLoading) {
         return <p>{prof.name}</p>
     } else return "Loading"
