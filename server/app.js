ac1e1e2b100cb43a85ed6ae972ce395a84e396aa
SetRedEyes <denidraven@gmail.com>
2022-02-01 12:12:08 +0100

    connected to mongoDV

--- server/app.js
+++ server/app.js
@@ -5,10 +5,33 @@ const chalk = require('chalk')
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 const PORT = config.get('port') ?? 8080
 
-app.listen(PORT, () =>
-  console.log(
-    chalk.green(`Server has been started on port ${PORT}`)
-  )
-)
+// if (process.env.NODE_ENV === 'production') {
+//   console.log('Production')
+// } else {
+//   console.log('Development')
+// }
+
+
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongoUri'))
+    console.log(chalk.green('MongoDB connected.'))
+    app.listen(PORT, () =>
+      console.log(
+        chalk.green(
+          `Server has been started on port ${PORT}`
+        )
+      )
+    )
+  } catch (e) {
+    console.log(chalk.red(e.message))
+    process.exit(1)
+  }
+}
+
+start()
