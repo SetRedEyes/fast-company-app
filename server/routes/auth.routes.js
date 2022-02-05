c83fc21ff53bb6d97a2e9bfc25244977f72a683d
SetRedEyes <denidraven@gmail.com>
2022-02-05 15:43:25 +0100

    implemented server login method

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -42,7 +42,7 @@ router.post('/signUp', [
           error: {
             message: 'EMAIL_EXISTS',
             code: 400,
-            errors:errors.array()
+            errors: errors.array()
           }
         })
       }
@@ -74,10 +74,75 @@ router.post('/signUp', [
   }
 ])
 
-router.post(
-  '/signInWithPassword',
-  async (req, response) => {}
-)
+//1. validate
+//2. find user
+//3. compare hashed password
+//4. generate token
+//5.return data
+
+router.post('/signInWithPassword', [
+  check('email', 'Email некорректный')
+    .normalizeEmail()
+    .isEmail(),
+  check('password', 'Пароль не может быть пустым').exists(),
+
+  async (req, res) => {
+    try {
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          error: {
+            message: 'INVALID_DATA',
+            code: 400
+          }
+        })
+      }
+
+      const { email, password } = req.body
+
+      const existingUser = await User.findOne({ email })
+
+      if (!existingUser) {
+        return res.status(400).send({
+          error: {
+            message: 'EMAIL_NOT_FOUND',
+            code: 400
+          }
+        })
+      }
+
+      const isPasswordEqual = await bcrypt.compare(
+        password,
+        existingUser.password
+      )
+
+      if (!isPasswordEqual) {
+        return res.status(400).json({
+          error: {
+            message: 'INVALID_PASSWORD',
+            code: 400
+          }
+        })
+      }
+
+      const tokens = tokenService.generate({
+        _id: existingUser._id
+      })
+      await tokenService.save(
+        existingUser._id,
+        tokens.refreshToken
+      )
+
+      res
+        .status(200)
+        .send({ ...tokens, userId: existingUser._id })
+    } catch (e) {
+      res
+        .status(500)
+        .json({ message: 'Server error. Try again later.' })
+    }
+  }
+])
 router.post('/token', async (req, response) => {})
 
 module.exports = router
