a8a8835e32c2873a7b076eea87cb5ea56fcc064a
SetRedEyes <denidraven@gmail.com>
2022-02-06 13:25:56 +0100

    imlemented user and coments server methods

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,6 +1,40 @@
 const express = require('express')
+const User = require('../models/User')
 const router = express.Router({ mergeParams: true })
+const auth = require('../middleware/auth.middleware')
 
+router.patch('/:userId', auth, async (req, res) => {
+  try {
+    const { userId } = req.params
 
+    // userId === current user id
+    if (userId === req.user._id) {
+      const updatedUser = await User.findByIdAndUpdate(
+        userId,
+        req.body,
+        //new - recieve const updateUser after database update
+        { new: true }
+      )
+      res.send(updatedUser)
+    } else {
+      res.status(401).json({ message: 'Unauthorized' })
+    }
+  } catch (e) {
+    res
+      .status(500)
+      .json({ message: 'Server error. Try again later.' })
+  }
+})
 
+router.get('/', auth, async (req, res) => {
+  try {
+    const list = await User.find()
+    //by default status =200 res.status(200).send...
+    res.send(list)
+  } catch (e) {
+    res
+      .status(500)
+      .json({ message: 'Server error. Try again later.' })
+  }
+})
 module.exports = router
