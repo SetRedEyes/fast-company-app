ea2d678af62c0e6fcbd4993b21783e8de1e23f52
SetRedEyes <denidraven@gmail.com>
2022-03-17 19:52:52 +0100

    8080

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(cors())
 
 app.use('/api', routes)
 
-const PORT = config.get('port') || 5000
+const PORT = config.get('port') ?? 8080
 
 if (process.env.NODE_ENV === 'production') {
   app.use(
